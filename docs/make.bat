61e4fa449d801669528522375787872d813119e2
Alexander Goncearenco <alexander.goncearenco@gmail.com>
2017-03-04 20:23:11 -0500

    Updated website

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=_build
+set BUILDDIR=..
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
@@ -73,10 +73,10 @@ if errorlevel 9009 (
 
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
+	echo.Build finished. The HTML pages are in %BUILDDIR%.
 	goto end
 )
 
--- docs/week5/index.rst
+++ docs/week5/index.rst
@@ -6,11 +6,9 @@ In week 5 we will work through a dataset and complete a regression task.
 
 
 
-
 Files
 -----
 
 Download the file below.
 
-* `Week 5 Regression and Classification <../Wk05-regression-classification.ipynb>`_
-* `Week 5 Regression and Classification with in class material <../Wk05-regression-classification-in-class.ipynb>`_
+* `Week 5 Regression <../Wk05-regression-classification-merged.ipynb>`_
